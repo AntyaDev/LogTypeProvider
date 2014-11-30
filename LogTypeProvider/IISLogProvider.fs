a2c409fc8a4d1f369f3e2689e879f766263ff913
AntyaDev <antyadev@gmail.com>
2014-11-30 18:04:14 +0200

    initial commit.

---
+++ LogTypeProvider/IISLogProvider.fs
@@ -0,0 +1,67 @@
+﻿namespace LogTypeProvider
+
+open ProviderImplementation.ProvidedTypes
+open Microsoft.FSharp.Core.CompilerServices
+open System.Reflection
+open System.IO
+
+[<AutoOpen>]
+module internal IISLogTypeCreation =
+ open System 
+ open TypeInference 
+ open IISLogReader
+
+ let erasedType<'T> assemblyName rootNamespace typeName = 
+     ProvidedTypeDefinition(assemblyName, rootNamespace, typeName, Some(typeof<'T>), HideObjectMethods = true)
+
+ let runtimeType<'T> typeName = 
+     ProvidedTypeDefinition(typeName, Some typeof<'T>, HideObjectMethods = true)
+
+ let property isStatic name ``type`` exprFunc =
+     ProvidedProperty(name, ``type``, IsStatic = isStatic, GetterCode = exprFunc)
+
+ let staticProperty name = property true name
+
+ let createMetaInfoProperty (mInfo: MetaInfo) =
+    staticProperty mInfo.Name mInfo.Type (TypeInference.getConversionQuotation(mInfo.Value, mInfo.Type))
+
+ let createMetaInfoProperties (mInfo: MetaInfo list) =
+    mInfo |> List.map(createMetaInfoProperty)
+    
+
+
+ let ``namespace`` = "LogTypeProvider.IIS"
+ let assembly = Assembly.GetExecutingAssembly() 
+ 
+ /// Create an erased type to represent IISLogProvider
+ let createIISLogProvider () =    
+    let iisLogType = erasedType<obj> assembly ``namespace`` "IISLogProvider"
+
+    let typeParams = [ProvidedStaticParameter("sample", typeof<string>)]    
+    
+    let initFunction (typeName: string) (parameterValues: obj[]) =            
+            match parameterValues with
+            | [| :? string as sample |] ->                
+
+                let iisLog = erasedType<obj> assembly ``namespace`` typeName         
+
+                let logStream = IISLogReader.initLogStream(sample)
+
+                IISLogReader.readIISLogSchema(logStream)
+                    |> Option.map(fun v -> v.MetaData |> createMetaInfoProperties)
+                    |> Option.iter(fun prop -> prop |> List.iter(iisLog.AddMember))
+
+                iisLog
+            | _ -> failwith "unexpected parameter values"
+
+    iisLogType.DefineStaticParameters(typeParams, initFunction)
+    iisLogType
+
+[<TypeProvider>]
+type IISLogProvider(providerConfig: TypeProviderConfig) as this =
+    inherit TypeProviderForNamespaces()
+        
+    do this.AddNamespace(``namespace``, [createIISLogProvider()])
+
+[<TypeProviderAssembly>]
+do()
---
+++ LogTypeProvider/TypeInference.fs
@@ -0,0 +1,35 @@
+﻿namespace LogTypeProvider
+
+module internal TypeInference =
+
+ open System.Globalization
+
+ let cultureInfo = CultureInfo.InvariantCulture
+ let dateTimeStyles = DateTimeStyles.AllowWhiteSpaces ||| DateTimeStyles.RoundtripKind
+
+ let convertDouble arg = System.Double.Parse(arg, NumberStyles.Currency, cultureInfo)
+ let convertInteger arg = System.Int32.Parse(arg)
+ let convertDateTime arg = System.DateTime.Parse(arg, cultureInfo, dateTimeStyles)
+
+ let toOption = function
+     | true, v  -> Some v
+     | false, _ -> None
+
+ let (|Date|_|) arg   = System.DateTime.TryParse(arg, cultureInfo, dateTimeStyles) |> toOption
+ let (|Int|_|) arg    = System.Int32.TryParse(arg) |> toOption
+ let (|Double|_|) arg = System.Double.TryParse(arg, NumberStyles.Currency, cultureInfo) |> toOption
+ 
+ let inferType arg = 
+     match arg with
+     | Date date   -> typeof<System.DateTime>
+     | Int integer -> typeof<int>
+     | Double dec  -> typeof<double>
+     | _           -> typeof<string>
+
+ let getConversionQuotation (arg: string, ``type``) : Quotations.Expr list -> Quotations.Expr =
+   if ``type`` = typeof<string> then (fun _ -> <@@ arg @@>)
+   elif ``type`` = typeof<int> then (fun _ -> <@@ convertInteger(arg) @@>)
+   elif ``type`` = typeof<double> then (fun _ -> <@@ convertDouble(arg) @@>)
+   elif ``type`` = typeof<System.DateTime> then (fun _ -> <@@ convertDateTime(arg) @@>)
+   else failwith "getConversionQuotation: Unsupported primitive type"
+
